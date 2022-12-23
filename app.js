64cbd45ba195c0dd4f02f124f2f9f7350095d9b7
elfbrtc <elif.barutcu.5353@hotmail.com>
2022-12-23 15:46:22 +0300

    -added Template Engine -created views folder. -edited HTML files.

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path=require('path');
+const ejs = require('ejs');
 
  const app = express();
+//Template engine
+app.set("view engine", "ejs");
 
+//Middleware
  app.use(express.static('public'))
 
- app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+//Routes
+ app.get('/about', (req, res) => {
+   res.render('about')
  })
+
+ app.get('/add', (req, res) => {
+  res.render('add')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
  const port = 3000;
  app.listen(port, ()=>{
